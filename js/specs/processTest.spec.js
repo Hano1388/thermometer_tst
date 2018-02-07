a1ef35ddada86fa4e175180749bbca3c63fd113b
Hano <hinsul3113@gmail.com>
2018-02-07 14:33:48 -0800

    Wrote a test to make sure user can specify a threshold

--- js/specs/processTest.spec.js
+++ js/specs/processTest.spec.js
@@ -11,4 +11,19 @@
       expect(showOutput([1, 3, 5, 6, 8])).toEqual([1, 33.8, 3, 37.4, 5, 41, 6, 42.8, 8, 46.4]);
     })
   });
+
+  describe('Threshold', () => {
+    // Setting class attribute inside 'specifyThreshold' function. It has nothing to do with this test so I am skipping it
+    var setAttribute = () => {
+      return true;
+    }
+
+    it('should be able to enter a threshold', async() => {
+      if (setAttribute()) {
+        pending();
+      }
+      let result = await provideThreshold();
+      expect(result).toBe();
+    })
+  });
 }());
